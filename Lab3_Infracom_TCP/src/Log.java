1c641f34349e70d16c75079709c0b04feef1dfe0
Andres Lugo <a.lugos@uniandes.edu.co>
2022-10-01 12:16:01 -0500

    Creación Logs
    Luisa se durmió

---
+++ Lab3_Infracom_TCP/src/Log.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Log {
+	
+	public void GenerarLog(String nomLog, String contenido) {	
+			
+	 File archivoCodigo;
+	 FileWriter escribir;
+	 PrintWriter linea;
+	 archivoCodigo = new File( "Logs/"+nomLog +".txt");
+	 
+	 if(!archivoCodigo.exists()) {
+		 try {
+			archivoCodigo.createNewFile(); 
+			escribir= new FileWriter(archivoCodigo, true);
+			linea = new PrintWriter(escribir);
+			linea.println(contenido);//ACA A DENTRO VA LA INFO EN STRING PARA EL ARCHIVO
+			linea.close();
+			escribir.close();
+		 } catch (Exception e) {}		
+	 
+	 } 
+	 
+	 else {
+		 try {
+			escribir= new FileWriter(archivoCodigo, true);
+			linea = new PrintWriter(escribir);
+			linea.println(contenido);//ACA A DENTRO VA LA INFO EN STRING PARA EL ARCHIVO
+			linea.close();
+			escribir.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
+		}
+	}
+	
+	public void removeLog(String nomLog) throws Throwable 	
+	{ 	
+        File archivoCodigo = new File(nomLog +".txt");
+        archivoCodigo.delete();
+	}
+	
+	public List<String> LeerLog(String nombreArchivo) throws Throwable
+	{
+	
+		File archivo;
+		FileReader leer;
+		
+		List<String> informacion = new ArrayList<>();
+				
+		archivo = new File(nombreArchivo+".txt");
+		
+			leer = new FileReader(archivo);
+			BufferedReader almacenamiento = new BufferedReader(leer);
+			
+			String cadena=almacenamiento.readLine();
+
+			while (cadena!=null)
+			{
+					if (cadena!=null && !cadena.equals(" ")) 
+					{
+						informacion.add(cadena);
+					}
+					cadena=almacenamiento.readLine();
+			}	
+		almacenamiento.close();
+		leer.close();
+		return informacion;
+	}
+    
+}
---
+++ Lab3_Infracom_TCP/src/main.java
@@ -0,0 +1,12 @@
+public class main {
+
+    private static Log logPrueba = new Log();
+
+    public static void main (String args[]) throws Throwable{
+		
+        System.out.println("Esto es una prueba de log");
+
+        logPrueba.GenerarLog("Jessica202013355", "Jeje\nSi sirve brrrr");
+
+	}
+}
